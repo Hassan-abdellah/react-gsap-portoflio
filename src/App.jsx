4a9daf01a74ee7446e84a8a393410fb4b9b68e6e
Hassan Abdellah <hassanabdellah25@gmail.com>
2025-11-23 18:13:09 +0200

    modify skills svgs and implement skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./Components/Navbar";
+import Hero from "./Sections/Hero";
+import Skills from "./Sections/Skills";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <Skills />
     </main>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,3 +12,38 @@ export const navbarLinks = [
     id: "gallery",
   },
 ];
+
+export const skills = [
+  {
+    title: "React JS",
+    icon: "/images/skills/reactjs.svg",
+  },
+  {
+    title: "Next JS",
+    icon: "/images/skills/nextjs.svg",
+  },
+  {
+    title: "Typescript",
+    icon: "/images/skills/typescript.svg",
+  },
+  {
+    title: "Tailwind CSS",
+    icon: "/images/skills/tailwindcss.svg",
+  },
+  {
+    title: "Shadcn",
+    icon: "/images/skills/shadcn.svg",
+  },
+  {
+    title: "Bootstrap",
+    icon: "/images/skills/bootstrap.svg",
+  },
+  {
+    title: "Ant Design",
+    icon: "/images/skills/antdesign.svg",
+  },
+  {
+    title: "Chakra UI",
+    icon: "/images/skills/chakraui.svg",
+  },
+];
